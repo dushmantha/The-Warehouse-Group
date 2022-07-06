152e72c5893ecd57c26d3369b66ba89a7b2eefa8
dushmantha <tdmihiran@gmail.com>
2022-07-06 22:11:26 +1200

    Initial Commit

---
+++ The Warehouse Group/The_Warehouse_GroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Warehouse_GroupApp.swift
+//  The Warehouse Group
+//
+//  Created by Tharaka Dushmantha on 6/07/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Warehouse_GroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ The Warehouse GroupUITests/The_Warehouse_GroupUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  The_Warehouse_GroupUITestsLaunchTests.swift
+//  The Warehouse GroupUITests
+//
+//  Created by Tharaka Dushmantha on 6/07/22.
+//
+
+import XCTest
+
+class The_Warehouse_GroupUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
